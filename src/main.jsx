2aca4318908b489c4c837640b82560184f54b93e
Neel200 <150600495+Neel200@users.noreply.github.com>
2025-09-25 19:34:04 +0530

    Updated files

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import AppContextProvider from "./Context/AppContext.jsx";
 import { BrowserRouter } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="RouterApp">
+  <BrowserRouter basename="Blogs">
     <AppContextProvider> 
       <App />
     </AppContextProvider>
